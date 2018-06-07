6e9a56041dc9dbbfa13890a37512b007b7637064
zariahlee4 <sneakiana@gmail.com>
2018-06-07 15:44:31 +0000

    automatically backed up by learn

---
+++ file .rb
@@ -0,0 +1,16 @@
+def name_length(value)
+if value <=3
+  puts"whats your name?"
+x= gets.chomp! .to_i
+if name_length =< 3
+  puts "your name is short"
+  elsif name_length =< 7
+  puts "your hellaaaaa average"
+else
+  puts"your name is longgggggggg"
+end
+
+
+name
+
+
